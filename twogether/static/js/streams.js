a7ea469272b65e7c3deff1b1568c8394e1b2aa79
pythonicforge <pythonicforge@gmail.com>
2025-02-16 10:26:48 +0530

    added: multiple user joins

--- twogether/static/js/streams.js
+++ twogether/static/js/streams.js
@@ -9,7 +9,34 @@ const client = AgoraRTC.createClient({ mode: "rtc", codec: "vp8" })
 let localTracks = []
 let remoteUsers = {}
 
+let handleUserJoin = async (user, mediaType) => {
+    remoteUsers[user.uid] = user
+    await client.subscribe(user, mediaType)
+
+    if(mediaType === "video") {
+        let player = document.getElementById(`user-container-${user.uid}`)
+        if(player != null){
+            player.remove()
+        }
+
+        player = `<div class="video-container" id="user-container-${user.uid}">
+                <div class="username-wrapper"><span class="user-name">User-2</span></div>
+                <div class="video-player" id="user-${user.uid}"></div>
+                </div>`
+        document.getElementById("video-streams").insertAdjacentHTML("beforeend", player)
+
+        user.videoTrack.play(`user-${user.uid}`)
+
+    }
+
+    if(mediaType === "audio") {
+        user.audioTrack.play()
+    }
+}
+
 let joinAndDisplayLocalStream = async () => {
+    client.on("user-published", handleUserJoin)
+
     UID = await client.join(APP_ID, CHANNEL, TOKEN, UID)
 
     localTracks = await AgoraRTC.createMicrophoneAndCameraTracks()
